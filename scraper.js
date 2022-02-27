5cddb73b632d588a9b2712a70bde93beab8ee150
alsino <iamahorse@gmail.com>
2022-02-27 22:55:18 +0100

    test

--- scraper.js
+++ scraper.js
@@ -1,20 +1,68 @@
-// Import request to make HTP requests
+// Import request to make HTTP requests
 // Import fs to save images to disk
 // Import chalk to highlight terminal output
 
-const request = require("request");
-const fs = require('fs');
-const chalk = require('chalk');
+import fs from 'fs'
+import chalk from 'chalk'
+import axios from 'axios';
+import cheerio from 'cheerio';
 
 
 console.log("hello")
 
-// // Define first and last image
-// let paintingNoStart = 1; //first image
-// let paintingNoEnd = 412; //last image
+// Define base URL
+const baseURL = `https://www.nytimes.com/`;
 
-// // Define base URL
-// let baseURL = `https://www.twoinchbrush.com/images/painting${paintingNoStart}.png`;
+getWebsiteContent(baseURL)
+
+async function getWebsiteContent(url) {
+  try {
+    const response = await axios.get(url)
+    const $ = cheerio.load(response.data)
+
+  
+  
+    const body = $('body');
+    // const content = body.find('.story-wrapper').find('>a').find('>h3').text()
+
+    // const content = body.find('.story-wrapper').find('a').find('h3').text()
+
+     const content = body.find('.story-wrapper').find('a').find('h3').find('.balancedHeadline').text()
+
+  
+
+    // const content = body.find('.story-wrapper').find('a').find('h3').find('span.balancedHeadline').find('a').find('h3').find('span.ghost').text()
+
+
+  //  div.story-wrapper > a > h3 > span.balancedHeadline > a > h3 > span.ghost
+
+
+
+
+    console.log(content)   
+    
+
+    // const table = $('table');
+    // const tableBody = table.find('tbody');
+    // const tableRow = tableBody.find('tr');
+
+      // Scrape each row in table
+      // tableRow.each((i, el) => {
+      //   const item = $(el)
+      //   let incidentLink = item.find('td').eq(6).find('ul').find('li').eq(0).find('a').attr('href');
+      //   let incidentURL = `${baseURL}${incidentLink}`;
+      //   console.log(chalk.yellow(`Getting page ${incidentURL}`));
+
+      //   // Get URLs
+      //   results.urls.push(incidentURL);
+           
+      // })   
+
+
+  } catch (error) {
+    console.error(error)
+  }
+}
 
 // // Define scraping function for each image at URL
 // const getImage = async (url, filename) => {
